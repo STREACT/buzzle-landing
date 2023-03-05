701c1a5d22336a8e176738b5f8848cdbb2fe0f25
OhYeHwan <azss0470@gmail.com>
2023-03-05 21:57:59 +0900

    feat: 1차 애니메이션 구현

--- src/components/Animation/index.tsx
+++ src/components/Animation/index.tsx
@@ -5,14 +5,24 @@ export default function Animation() {
     <div className={styles.container}>
       <div className={styles.box}>
         <p className={styles.sayHello}>HELLO, WE ARE</p>
-        <p className={styles.buzzle}>
-          <span className={styles.buzzleB}>B</span>
-          <span className={styles.buzzleU}>U</span>
-          <span className={styles.buzzleZ}>Z</span>
-          <span className={styles.buzzleZZ}>Z</span>
-          <span className={styles.buzzleL}>L</span>
-          <span className={styles.buzzleE}>E</span>
-        </p>
+        <div className={styles.buzzle}>
+          <span className={styles.moveFarRight}>
+            <span className={styles.buzzleB}>B</span>
+            <span className={styles.buzzleU}>U</span>
+          </span>
+          <span className={styles.moveSecondSeat}>
+            <span className={styles.buzzleZ}>Z</span>
+          </span>
+          <span className={styles.moveFourthSeat}>
+            <span className={styles.buzzleZZ}>Z</span>
+          </span>
+          <span className={styles.moveFirstSeat}>
+            <span className={styles.buzzleL}>L</span>
+          </span>
+          <span className={styles.moveThirdSeat}>
+            <span className={styles.buzzleE}>E</span>
+          </span>
+        </div>
       </div>
     </div>
   );
